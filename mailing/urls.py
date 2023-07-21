b4d6ae565fee3e102b7941a2f812b30e890d2eca
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-07-21 10:18:00 +0300

    MailSettingList ok

--- mailing/urls.py
+++ mailing/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from mailing.apps import MailingConfig
-from mailing.views import ClientList, ClientCreate, MainPage, ClientDetail, ClientDelete
+from mailing.views import ClientList, ClientCreate, MainPage, ClientDetail, ClientDelete, MailSettingList, \
+    MailSettingCreate, MailSettingDetail, MailSettingDelete
 
 app_name = MailingConfig.name
 
@@ -14,7 +15,7 @@ urlpatterns = [
     path('client_detail/<int:pk>/', ClientDetail.as_view(), name='client_detail'),
     path('client_delete/<int:pk>/', ClientDelete.as_view(), name='client_delete'),
     path('mailsetting_list/', MailSettingList.as_view(), name='mail-setting-list'),
-    # path('mailsetting_create/', MailSettingCreate.as_view(), name='mail-setting-create'),
-    # path('mailsetting_detail/<int:pk>/', mailsettingDetailView.as_view(), name='mail-setting-detail'),
-    # path('mailsetting_delete/<int:pk>/', mailsettingDeleteView.as_view(), name='mail-setting-delete'),
+    path('mailsetting_create/', MailSettingCreate.as_view(), name='mail-setting-create'),
+    path('mailsetting_detail/<int:pk>/', MailSettingDetail.as_view(), name='mail-setting-detail'),
+    path('mailsetting_delete/<int:pk>/', MailSettingDelete.as_view(), name='mail-setting-delete'),
 ]
